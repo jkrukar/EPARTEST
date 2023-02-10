4cd1c5369e84157e1243d1d05fee5a344b8dcb9b
John Krukar <john.a.krukar@gmail.com>
2023-02-10 16:25:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,7 @@ export default function Home() {
   return (
     <>
     
-      <script src="https://aframe.io/releases/1.0.0/aframe.min.js"></script>
-    <!-- we import arjs version without NFT but with marker + location based support -->
+    <script src="https://aframe.io/releases/1.0.0/aframe.min.js"></script>
     <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
     <body style="margin : 0px; overflow: hidden;">
         <a-scene embedded arjs>
@@ -25,7 +24,7 @@ export default function Home() {
         <a-entity camera></a-entity>
         </a-scene>
     </body>
-    
+
     </>
   )
 }

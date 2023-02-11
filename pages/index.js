394d3fe8c33e25336a4dbdc28cb4224e28922f6c
John Krukar <john.a.krukar@gmail.com>
2023-02-11 05:22:09 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    
-    <script src='https://aframe.io/releases/1.0.0/aframe.min.js'></script>
-    <script src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'></script>
-    // <body style="margin : 0px; overflow: hidden;">
-        <a-scene embedded arjs>
-        <a-marker preset="hiro">
-            <a-entity
-            position="0 0 0"
-            scale="0.05 0.05 0.05"
-            gltf-model="your-server/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-            ></a-entity>
-        </a-marker>
-        <a-entity camera></a-entity>
-        </a-scene>
-    // </body>
+    <p>test</p>
+
 
     </>
   )

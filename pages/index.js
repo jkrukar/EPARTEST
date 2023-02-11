23bec20f28d237ed641ad91e15a5fe8159c9250b
John Krukar <john.a.krukar@gmail.com>
2023-02-11 05:20:14 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     
     <script src='https://aframe.io/releases/1.0.0/aframe.min.js'></script>
     <script src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'></script>
-    <body style="margin : 0px; overflow: hidden;">
+    // <body style="margin : 0px; overflow: hidden;">
         <a-scene embedded arjs>
         <a-marker preset="hiro">
             <a-entity
@@ -22,7 +22,7 @@ export default function Home() {
         </a-marker>
         <a-entity camera></a-entity>
         </a-scene>
-    </body>
+    // </body>
 
     </>
   )

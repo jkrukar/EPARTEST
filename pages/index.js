88b6264e8ed034cb1c4352debd92ea103b3c035f
John Krukar <john.a.krukar@gmail.com>
2023-02-17 16:20:00 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,7 @@ export default function Home() {
         <div style="margin : 0px; overflow: hidden;">
     <a-scene embedded arjs>
       <a-marker preset="hiro">
-        <a-entity
-          position="0 0 0"
-          scale="0.05 0.05 0.05"
-          gltf-model="arTestCube.gltf"
-        ></a-entity>
+
       </a-marker>
       <a-entity camera></a-entity>
     </a-scene>

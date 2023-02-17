1556bd595db6d4f5de6bed0f410cd40f3b6ca7de
John Krukar <john.a.krukar@gmail.com>
2023-02-17 16:06:03 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,10 @@ import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
+  	<>
   	<Script src="/aframe-ar.js" />
   	<Script src="/aframe.min.js" />
   	<Component {...pageProps} />
+  	</>
   	)
 }

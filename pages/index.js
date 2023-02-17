1bbe7d1deff5a48dc970ceb15ed1e29d3ff1aca2
John Krukar <john.a.krukar@gmail.com>
2023-02-17 16:19:02 -0700

    test cube

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,21 @@ export default function Home() {
     <main className={styles.main}>
 
     <p>test</p>
+
+
+        <div style="margin : 0px; overflow: hidden;">
+    <a-scene embedded arjs>
+      <a-marker preset="hiro">
+        <a-entity
+          position="0 0 0"
+          scale="0.05 0.05 0.05"
+          gltf-model="arTestCube.gltf"
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+  </div>
+
     </main>
     </>
   )
